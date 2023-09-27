d557d0cf82843e0a516bcc7c0d0d01e7b89010bf
Benjamín Ortega´ <benjaminortega.mtnez@gmail.com>
2023-09-27 01:48:56 -0600

    encriptación de password

--- src/main/java/com/lcp/app/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/lcp/app/service/impl/CustomerServiceImpl.java
@@ -3,6 +3,7 @@ package com.lcp.app.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.lcp.app.entity.Customer;
@@ -12,64 +13,70 @@ import com.lcp.app.service.CustomerService;
 @Service
 public class CustomerServiceImpl implements CustomerService{
 
-	@Autowired
-	CustomerRepository customerRepository;
-	
-	@Override
-	public Customer createCustomer(Customer customer) {
-		return saveCustomer(customer);
-	
-	}
-	
-	public Customer saveCustomer(Customer customer) {
-		return customerRepository.save(customer);
-	}
+    @Autowired
+    CustomerRepository customerRepository;
+    
+    @Override
+    public Customer createCustomer(Customer customer) {
+       return saveCustomer(customer);
+    
+    }
 
-	@Override
-	public Customer getCustomerById(Long id) {
-		return customerRepository.findById(id)
-				.orElseThrow( ()-> new IllegalStateException("Customer does not exist with id " + id) );
-	}
-	
-	public List <Customer> getCustomerByName(String name) {
-		List <Customer> existingCustomers = customerRepository.findByName(name);
-				if( existingCustomers == null || existingCustomers.isEmpty()) {
-					throw new IllegalStateException("User does not exist");
-				}
-				return existingCustomers;
-	}
-	
-	public List <Customer> getCustomerByLastName(String name) {
-		List <Customer> existingCustomers = customerRepository.findByLastName(name);
-				if( existingCustomers == null || existingCustomers.isEmpty()) {
-					throw new IllegalStateException("User does not exist");
-				}
-				return existingCustomers;
-	}
+    public Customer saveCustomer(Customer customer) {
+       String encryptedPassword = bcrypt.encode(customer.getPassword());
+       customer.setPassword(encryptedPassword);
+       return customerRepository.save(customer);
+    }
 
-	@Override
-	public List<Customer> getAllCustomers() {
-		return (List<Customer>) customerRepository.findAll();
-	}
+    @Override
+    public Customer getCustomerById(Long id) {
+       return customerRepository.findById(id)
+             .orElseThrow( ()-> new IllegalStateException("Customer does not exist with id " + id) );
+    }
+    
+    public List <Customer> getCustomerByName(String name) {
+       List <Customer> existingCustomers = customerRepository.findByName(name);
+             if( existingCustomers == null || existingCustomers.isEmpty()) {
+                throw new IllegalStateException("User does not exist");
+             }
+             return existingCustomers;
+    }
+    
+    public List <Customer> getCustomerByLastName(String name) {
+       List <Customer> existingCustomers = customerRepository.findByLastName(name);
+             if( existingCustomers == null || existingCustomers.isEmpty()) {
+                throw new IllegalStateException("User does not exist");
+             }
+             return existingCustomers;
+    }
 
-	@Override
-	public Customer updateCustomer(Customer customer, Long id) {
-		Customer existingCustomer = getCustomerById(id);
-		existingCustomer.setName(  customer.getName()   );
-		existingCustomer.setLastName( customer.getLastName());
-		existingCustomer.setLastName2( customer. getLastName2());
-		existingCustomer.setSex( customer.getSex());
-		existingCustomer.setBirthDate( customer.getBirthDate());
-		existingCustomer.setEmail(customer.getEmail());
-		existingCustomer.setPhonenumber( customer.getPhonenumber());
-		return saveCustomer( existingCustomer );
-	}
+    @Override
+    public List<Customer> getAllCustomers() {
+       return (List<Customer>) customerRepository.findAll();
+    }
 
-	@Override
-	public Customer deleteCustomer(Long id) {
-		Customer existingCustomer = getCustomerById(id);
-		customerRepository.delete(existingCustomer);
-		return existingCustomer;
-	}
+    @Override
+    public Customer updateCustomer(Customer customer, Long id) {
+       Customer existingCustomer = getCustomerById(id);
+       existingCustomer.setName(  customer.getName()   );
+       existingCustomer.setLastName( customer.getLastName());
+       existingCustomer.setLastName2( customer. getLastName2());
+       existingCustomer.setSex( customer.getSex());
+       existingCustomer.setBirthDate( customer.getBirthDate());
+       existingCustomer.setEmail(customer.getEmail());
+       existingCustomer.setPhonenumber( customer.getPhonenumber());
+       String encryptedPassword = bcrypt.encode(customer.getPassword());
+       existingCustomer.setPassword(encryptedPassword);
+       return saveCustomer( existingCustomer );
+    }
 
+
+    @Override
+    public Customer deleteCustomer(Long id) {
+       Customer existingCustomer = getCustomerById(id);
+       customerRepository.delete(existingCustomer);
+       return existingCustomer;
+    }
+
+    BCryptPasswordEncoder bcrypt=new BCryptPasswordEncoder();
 }
