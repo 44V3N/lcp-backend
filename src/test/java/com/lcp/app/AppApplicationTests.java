1df59b822431bddfd846f2deb157ebfa81756db1
davidOlea <luis.david.olea.mtz@gmail.com>
2023-09-27 23:15:08 -0600

    lo que quieras :v

--- src/test/java/com/lcp/app/AppApplicationTests.java
+++ src/test/java/com/lcp/app/AppApplicationTests.java
@@ -1,13 +1,11 @@
 package com.lcp.app;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	
+	
 
 }
