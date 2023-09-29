65420a80649f64ee01df5b23c618e8deaaccc6ae
Mario Pech <pech.mivan@gmail.com>
2023-09-28 23:51:37 -0600

    se agrega el cross origins

--- src/main/java/com/lcp/app/AppApplication.java
+++ src/main/java/com/lcp/app/AppApplication.java
@@ -1,7 +1,13 @@
 package com.lcp.app;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 public class AppApplication {
@@ -9,5 +15,17 @@ public class AppApplication {
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
     }
+    
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins( List.of("http://127.0.0.1:5173") );
+		configuration.setAllowedMethods( List.of("GET", "POST", "PUT", "DELETE") );
+		configuration.setAllowedHeaders( List.of("Authorization","Content-Type") );
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+		
+	}
     
 }
